4137d80f9e45a0ec1b559ad0c80231a67dbcc61e
M1ch4l11 <lojko.michal11@gmail.com>
2024-10-16 18:43:02 +0200

    Fix: Add id for update event

--- src/models/Form.ts
+++ src/models/Form.ts
@@ -9,8 +9,9 @@ export interface FormProps {
 }
 
 export const formSchema = z.object({
+  id: z.string().optional(),
   title: z.string().min(5, "Title is required"),
-  description: z.string().optional(),
+  description: z.string(),
   deadline: z.string(),
   Tasks_CategoryId: z.string().min(1, "Task Category is required"),
 });
